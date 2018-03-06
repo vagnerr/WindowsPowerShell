322444c527fd6173bc6cc9c9edf9e75f3f0f91f8
Peter Wise <peter@bloodaxe.com>
2018-03-06 15:04:36 +0000

    RC1 copy current items from Shirka

---
+++ profile.ps1
@@ -0,0 +1,3 @@
+# Thanks to https://stackoverflow.com/questions/8264655/how-to-make-powershell-tab-completion-work-like-bash
+# Propper tab completion :)
+Set-PSReadlineKeyHandler -Key Tab -Function Complete
