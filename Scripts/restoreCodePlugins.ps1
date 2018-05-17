6afb9465f2b33125e5d48db54b0a6ec0009c5f4d
Peter Wise <peter@bloodaxe.com>
2018-05-17 18:45:06 +0100

    Fix Lint issues

--- Scripts/restoreCodePlugins.ps1
+++ Scripts/restoreCodePlugins.ps1
@@ -1,6 +1,8 @@
 # Restore VS Code plugins backed up with getCodePlugins.ps
 # (https://stackoverflow.com/questions/35773299/how-can-you-export-vs-code-extension-list)
+#
+# Note: running code.cmd explicitly in case PSCode has been installed.
 
 $data_location = (Split-Path -Parent $script:MyInvocation.MyCommand.Path)
 
-cat $data_location\data\code_extensions.list |% { code.cmd --install-extension $_}
+Get-Content $data_location\data\code_extensions.list |ForEach-Object { code.cmd --install-extension $_}
