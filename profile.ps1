849ac6716044e3478956dfa2bf9ac97ff6d285e7
Peter Wise <peter@bloodaxe.com>
2018-05-17 11:20:10 +0100

    Adding PSCode to fix issue with VNC paths
    https://github.com/Microsoft/vscode/issues/14529

--- profile.ps1
+++ profile.ps1
@@ -1,3 +1,12 @@
 # Thanks to https://stackoverflow.com/questions/8264655/how-to-make-powershell-tab-completion-work-like-bash
 # Propper tab completion :)
 Set-PSReadlineKeyHandler -Key Tab -Function Complete
+
+# Fixup VSCode with a code.ps1 rather than code.cmd so that it can
+# cope with UNC paths see...
+#   https://github.com/Microsoft/vscode/issues/14529
+# uses utility provided by 3rd party...
+#   https://github.com/wtjones/PSCode (https://www.powershellgallery.com/packages/PSCode/1.0.7)
+# does need the PSCode installed as administrator first...
+#   C:\WINDOWS\system32> Install-Module -Name PSCode
+Import-Module PSCode
